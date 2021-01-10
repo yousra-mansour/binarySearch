03202576741d69418858077375acff6519839903
yousra-mansour <yousra.m.mansour@gmail.com>
2021-01-10 18:32:23 +0200

    the code

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,31 +1,35 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 16
-VisualStudioVersion = 16.0.30621.155
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}") = "binary search", "binary search\binary search.vcxproj", "{2BFDE9F0-454A-4837-9B84-E0188A8E440A}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|x64 = Debug|x64
-		Debug|x86 = Debug|x86
-		Release|x64 = Release|x64
-		Release|x86 = Release|x86
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{2BFDE9F0-454A-4837-9B84-E0188A8E440A}.Debug|x64.ActiveCfg = Debug|x64
-		{2BFDE9F0-454A-4837-9B84-E0188A8E440A}.Debug|x64.Build.0 = Debug|x64
-		{2BFDE9F0-454A-4837-9B84-E0188A8E440A}.Debug|x86.ActiveCfg = Debug|Win32
-		{2BFDE9F0-454A-4837-9B84-E0188A8E440A}.Debug|x86.Build.0 = Debug|Win32
-		{2BFDE9F0-454A-4837-9B84-E0188A8E440A}.Release|x64.ActiveCfg = Release|x64
-		{2BFDE9F0-454A-4837-9B84-E0188A8E440A}.Release|x64.Build.0 = Release|x64
-		{2BFDE9F0-454A-4837-9B84-E0188A8E440A}.Release|x86.ActiveCfg = Release|Win32
-		{2BFDE9F0-454A-4837-9B84-E0188A8E440A}.Release|x86.Build.0 = Release|Win32
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {34333678-D499-495F-AEA1-77B1EE0F3E2B}
-	EndGlobalSection
-EndGlobal
+﻿#include<iostream>
+#include<array>
+using namespace std;
+
+int binarySearch(int a[], int key ,int sizearr);
+
+int main() {
+
+	int a[] = { 0,1,2,3,4,5,6,7,8,9};
+    cout << binarySearch(a, 9, size(a) - 1);
+
+	return 0;
+}
+
+int binarySearch(int a[], int key, int sizearr)
+{
+	int low = 0;
+	int hight = sizearr;
+	while (low <= hight)
+	{
+		int mid = low + ((hight - low)/ 2);
+		if (key < a[mid]) 
+		{ 
+			hight = mid - 1; 
+		}
+		else if (key > a[mid]) 
+		{
+			low = mid + 1;
+		} 
+		else 
+			return mid;
+
+	}
+	return -1;
+}
